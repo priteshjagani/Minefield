c572258cda599dbcaf892508792c3360f1736ac6
priteshjagani <priteshjagani@gmail.com>
2016-03-07 10:15:28 -0800

    Added an incorrect case for placeMine to see if it is working

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -14,11 +14,30 @@ class FieldTest : public ::testing::Test
 		virtual void SetUp(){}
 		virtual void TearDown(){}
 };
+//Function to be tested in isSafe()
+
+
+// Following are the test cases.
 
 TEST(FieldTest, placeMineInBounds)
 {
 	Field minefield;
-	
+	minefield.placeMine(4,5);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+}
+
+//Just to test if this is working in incorrect case
+TEST(FieldTest, MineInBounds)
+{
+	Field minefield;
+	minefield.placeMine(0,0);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+}
+
+
+TEST(FieldTest, isSafeCase)
+{
+	Field minefield;
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
 }
